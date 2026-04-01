ac0d292823c62d1cad585919e9ee1796b2cbc1a1
Siddharth <konsiddharth@gmail.com>
2026-04-01 21:42:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+async function getWeather() {
+    const city = document.getElementById("cityInput").value;
+    const loading = document.getElementById("loading");
+    const weatherDiv = document.getElementById("weatherData");
 
+    if (city === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    loading.style.display = "block";
+    weatherDiv.innerHTML = "";
+
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+
+        const data = await response.json();
+
+        loading.style.display = "none";
+
+        weatherDiv.innerHTML = `
+            <h2>${data.name}</h2>
+            <p>Temperature: ${data.main.temp} °C</p>
+            <p>Weather: ${data.weather[0].main}</p>
+        `;
+
+    } catch (error) {
+        loading.style.display = "none";
+        weatherDiv.innerHTML = "Error fetching weather data";
+    }
+}
